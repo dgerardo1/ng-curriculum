d6deaa52e897559d9f678e9859657efed19f3d0b
Gerardo Hernandez Flores <dgerardohernandez@gmail.com>
2025-05-19 16:21:55 -0600

    Update job experience

--- src/app/components/work-experience/work-experience.component.ts
+++ src/app/components/work-experience/work-experience.component.ts
@@ -9,59 +9,82 @@ export class WorkExperienceComponent {
 
   jobs = [
     {
+      title: 'Especialista De Desarrollo IT Sr',
+      company: 'Audi México',
+      sector: 'Automotriz',
+      modality: 'Híbrido',
+      startDate: 'Nov. 2023',
+      endDate: 'Actualidad',
+      activities: [
+                    'Transformo necesidades en soluciones técnicas liderando el levantamiento de requerimientos.',
+                    'Superviso y apruebo documentación para garantizar calidad y cumplimiento.',
+                    'Aseguro la correcta implementación del desarrollo, alineado a los objetivos del proyecto.',
+                    'Guío al equipo con metodologías y tecnologías actuales para optimizar resultados.',
+                    'Analizo y superviso temas de soporte técnico para garantizar eficiencia.',
+                    'Coordino mantenimiento y actualizaciones para estabilidad y mejora continua.',
+                    'Establezco niveles de responsabilidad para una estructura clara y efectiva.',
+                    'Elaboro propuestas técnicas y económicas alineadas con objetivos estratégicos.',
+                    'Gestiono proveedores de desarrollo para optimizar colaboración y eficiencia.'
+                  ]
+    },
+    {
       title: 'Front-end Developer',
       company: 'Smart Plus',
+      sector: 'Fintech',
       modality: 'Remoto',
       startDate: 'Dic. 2020',
       endDate: 'Mar. 2023',
       activities: [
-        'Gestioné las actividades del equipo de frot end.',
-        'Realicé desarrollo web front end.',
-        'Maqueté interfaces de usuario web.',
-        'Analicé y diseñé productos web (UX/UI).',
-        'Realicé mantenimiento y actualización de sitios y aplicaciones web.',
-      ]
+                    'Gestioné las actividades del equipo de front-end, asegurando eficiencia y calidad en los entregables.',
+                    'Desarrollé interfaces web interactivas y optimizadas para mejorar la experiencia del usuario.',
+                    'Maqueté interfaces de usuario, aplicando principios de accesibilidad y diseño responsivo.',
+                    'Analicé y diseñé productos digitales con enfoque UX/UI, mejorando la usabilidad y funcionalidad.',
+                    'Mantuve y actualicé sitios y aplicaciones web, garantizando estabilidad y mejoras continuas.'
+                  ]
     },
     {
       title: 'Diseñador UX|UI y Front-end Developer',
       company: 'MyCaribu',
+      sector: 'insurtech',
       modality: 'Remoto',
       startDate: 'Ene. 2020',
       endDate: 'Dic. 2020',
       activities: [
-        'Analicé y diseñé interfaces de usuario móvil.',
-        'Analicé y diseñé el sitio web.',
-        'Maqueté la interfaz de usuario para la web.',
-        'Realicé mantenimiento y actualización del sitio web.',
-      ]
+                    'Diseñé y optimicé interfaces de usuario para aplicaciones móviles, mejorando usabilidad y accesibilidad.',
+                    'Analicé y estructuré el sitio web, asegurando una experiencia intuitiva y funcional.',
+                    'Maqueté interfaces web con un enfoque responsivo y adaptado a estándares de diseño.',
+                    'Mantuve y actualicé el sitio web, garantizando estabilidad y mejoras continuas.'
+                  ]
     },
     {
       title: 'Webmaster',
       company: 'Edicomm',
+      sector: 'Editorial',
       modality: 'Remoto',
       startDate: 'May. 2016',
       endDate: 'Mar. 2019',
       activities: [
-        'Diseñé y desarrollé sitios web.',
-        'Diseñé y gestioné contenido para web y redes sociales.',
-        'Gestioné las actividades y recursos del área (humanos y económicos).',
-        'Realicé mantenimiento y actualización de sitios web.',
-        'Me encargué de la gestión de proveedores.',
-      ]
+                    'Diseñé y desarrollé sitios web, asegurando funcionalidad y experiencia óptima.',
+                    'Creé y gestioné contenido digital para web y redes sociales, optimizando alcance.',
+                    'Administré actividades y recursos del área, optimizando procesos y eficiencia.',
+                    'Mantuve y actualicé sitios web, garantizando estabilidad y mejoras continuas.',
+                    'Coordiné la gestión de proveedores, asegurando calidad y cumplimiento de servicios.'
+                  ]
     },
     {
       title: 'Analista de Sistemas Sr',
       company: 'Adecco México',
+      sector: 'Recursos Humanos',
       modality: 'Presencial',
       startDate: 'May. 2008',
       endDate: 'May. 2016',
       activities: [
-        'Lideré el proyecto intranet Adecco México.',
-        'Lideré el proyecto intranet Adecco LATAM.',
-        'Lideré el proyecto de Sitio Web en 2009.',
-        'Realicé supervisión y mantenimiento de sitios y aplicaciones web.',
-        'Analicé y diseñé productos web (UX/UI).',
-      ]
+                    'Dirigí el proyecto de intranet para Adecco México, asegurando su implementación eficiente.',
+                    'Lideré el desarrollo de la intranet Adecco LATAM, optimizando la gestión interna.',
+                    'Coordiné el proyecto de sitio web en 2009, garantizando su diseño y funcionalidad.',
+                    'Supervisé y mantuve sitios y aplicaciones web, asegurando estabilidad y mejoras constantes.',
+                    'Diseñé y optimicé productos web con enfoque UX/UI, mejorando la experiencia del usuario.'
+                  ]
     }
   ]
 
